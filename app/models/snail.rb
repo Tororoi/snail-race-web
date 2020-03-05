9bf311ce33b6af566091ac2f36b5ecd0129ba0a8
Tororoi <cantwell.tom@gmail.com>
2020-03-04 19:46:15 -0500

    Polished animations and allowed for programmable attributes

--- app/models/snail.rb
+++ app/models/snail.rb
@@ -5,4 +5,12 @@ class Snail < ApplicationRecord
   has_many :races, through: :snail_races
 
   validates :name, presence: true
+
+  def speed(race)
+    if self.favorite_food == race.prize.name
+      rand(13.0..16.0) - 1 
+    else
+      rand(13.0..16.0)
+    end
+  end
 end
