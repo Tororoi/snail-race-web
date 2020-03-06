0b5f740161cfa9fda73f32b807f4c1b264ffe509
Tororoi <cantwell.tom@gmail.com>
2020-03-06 14:46:36 -0500

    added nav bar

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -4,6 +4,8 @@ class BetsController < ApplicationController
     
     def create
         @bet = Bet.create(bet_params)
+        @user = User.find(session[:user_id])
+        @user.update(money: 6)
         if @bet.valid?
             redirect_to @bet.snail_race.race
         else
