dea6b58ac114f6b12fca81cbb7208094d366f7f8
Tororoi <cantwell.tom@gmail.com>
2020-03-10 15:40:40 -0400

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,9 +63,7 @@ SnailRace.create(snail_id: blue_light.id, race_id: race_two.id, odds: 0.3, rank:
 SnailRace.create(snail_id: deep_purple.id, race_id: race_two.id, odds: 0.2, rank: 2)
 SnailRace.create(snail_id: dark_forest.id, race_id: race_two.id, odds: 0.1, rank: 4)
 
-first_bet = Bet.create(snail_race_id: default.id, user_id: fika.id, amount: 5)
-second_bet = Bet.create(snail_race_id: blue_light.id, user_id: tom.id, amount: 10)
-third_bet = Bet.create(snail_race_id: dark_forest.id, user_id: tom.id, amount: 12)
+
 
 
 
